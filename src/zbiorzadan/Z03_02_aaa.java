25e31844fa0e39660e8b1b2d00e16c62540dd0ac
Dawid <dawid.orlinski1916@gmail.com>
2020-12-02 18:27:59 +0100

    new version

--- src/zbiorzadan/Z03_02_aaa.java
+++ src/zbiorzadan/Z03_02_aaa.java
@@ -1,2 +1,11 @@
-package zbiorzadan;public class Z03_02_aaa {
+package zbiorzadan;
+
+public class Z03_02_aaa {
+    public static void main (String[] args) {
+        char[] wordInformationTechnology = {'I','n','f','o','r','m','a','t','y','k','a'};
+        for (char ch : wordInformationTechnology) {
+            System.out.println(ch);
+        }
+
+    }
 }
