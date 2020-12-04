5764949344bdb7c343afd35f15a852b681af4547
Dawid <dawid.orlinski1916@gmail.com>
2020-12-04 19:47:40 +0100

    Z03_02_aa - refactor

--- src/zbiorzadan/Z03_02_aa.java
+++ src/zbiorzadan/Z03_02_aa.java
@@ -2,12 +2,11 @@ package zbiorzadan;
 
 public class Z03_02_aa {
     public static void main(String[] args) {
-        String[] goodMorning = {"Dzień dobry"};{
+        String[] goodMorning = {"Dzień dobry"};
         System.out.println(goodMorning[0]);
-        }
 
-    char[] goodMorningSecondChoice = {'d', 'z', 'i', 'e', 'n', ' ', 'd', 'o', 'b', 'r', 'y'};
-        for (int i = 0; i < goodMorningSecondChoice.length; i++ ) {
+        char[] goodMorningSecondChoice = {'d', 'z', 'i', 'e', 'n', ' ', 'd', 'o', 'b', 'r', 'y'};
+        for (int i = 0; i < goodMorningSecondChoice.length; i++) {
             System.out.println(goodMorningSecondChoice[i]);
         }
 
